66df2136051fb0ab62c464d099eec239161d6155
Micha de Vries <contact@michadevries.nl>
2021-12-06 16:23:58 +0100

    Initial module file

---
+++ pb_entry.php
@@ -0,0 +1,16 @@
+<?php
+    namespace Module;
+
+    class Scoreboard {
+        public function initialize() {
+
+        }
+
+        public function requestHandler() {
+
+        }
+
+        public function configurator() {
+
+        }
+    }
