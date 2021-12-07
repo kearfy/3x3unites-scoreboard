93802a40d89ea956b79bf5068058ec9c3b687467
Hesselaar23 <81434106+Hesselaar23@users.noreply.github.com>
2021-12-07 14:24:17 +0100

    sorry message fixed

--- functions/public_stats.php
+++ functions/public_stats.php
@@ -1,3 +1,23 @@
 <?php
-    echo 'We\'re sorry. This website is not ready yet but will be ready shortly.';
+
 ?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Gegevens aanvullen - 3x3unites scoreboard</title>
+        
+        <link rel="stylesheet" href="<?php echo SITE_LOCATION; ?>/pb-loader/module-static/scoreboard/default.css">
+        <link rel="stylesheet" href="<?php echo SITE_LOCATION; ?>/pb-loader/module-static/scoreboard/pages.css">
+    </head>
+    <body>
+        <p>
+            <div class="notready">
+                &#9814;We're sorry. This website is not ready yet but will be ready shortly.&#9814;
+            </div>
+        </p>
+    </body>
+</html>
