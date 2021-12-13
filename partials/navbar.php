9aa4ab6b88b86e044c3314bcdb20503bafc120b2
Micha de Vries <contact@michadevries.nl>
2021-12-13 14:09:47 +0100

    Added centralised navbar.

---
+++ partials/navbar.php
@@ -0,0 +1,32 @@
+<?php
+    $controller = new Library\Controller;
+    $user = $controller->__model('user');
+    $signedin = $user->signedin();
+
+    function showSignedin($signedin, $content) {
+        if ($signedin) {
+            echo $content;
+        }
+    }
+
+    function showSignedout($signedin, $content) {
+        if (!$signedin) {
+            echo $content;
+        }
+    }
+?>
+
+<nav>
+    <section class="left">
+        <?php showSignedout($signedin, '<a href="/signup">Registreren</a>'); ?>
+    </section>
+    <section class="middle">
+        <div class="logo-container">
+            <img src="<?php echo SITE_LOCATION; ?>/pb-loader/module-static/scoreboard/logo_white.svg" alt="3x3unites logo">
+        </div>
+    </section>
+    <section class="right">
+        <?php showSignedout($signedin, '<a href="/signin">Inloggen</a>'); ?>
+    </section>
+</nav>
+
