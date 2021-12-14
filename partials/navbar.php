e8a54284ab75e3e692bede3365d914f857a3e161
Hesselaar23 <81434106+Hesselaar23@users.noreply.github.com>
2021-12-14 12:59:17 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -19,6 +19,7 @@
 <nav>
     <section class="left">
         <?php showSignedout($signedin, '<a href="/signup">Registreren</a>'); ?>
+        <?php showSignedin($signedin, '<a href="/inschrijven">Inschrijven</a>'); ?>
     </section>
     <section class="middle">
         <div class="logo-container">
