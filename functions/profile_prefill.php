03053fcbef36e903b72f6a54fbbe24ba9fe77b44
Micha de Vries <contact@michadevries.nl>
2021-12-17 15:25:11 +0100

    Updated profile_prefill required fields.

--- functions/profile_prefill.php
+++ functions/profile_prefill.php
@@ -21,7 +21,7 @@
     }
 
     $postdata = Request::parsePost();
-    $dataset = array('gender', 'age', 'height', 'club', 'team', 'competition');
+    $dataset = array('gender', 'age', 'height', 'club', 'team');
     $allowed = array('gender', 'age', 'height', 'club', 'team', 'competition', 'notes');
 
     if (Request::method() == 'POST') {
@@ -34,7 +34,7 @@
             $users->metaSet($user->id, 'height', $postdata->height);
             $users->metaSet($user->id, 'club', $postdata->club);
             $users->metaSet($user->id, 'team', $postdata->team);
-            $users->metaSet($user->id, 'competition', $postdata->competition);
+            if (isset($postdata->competition)) $users->metaSet($user->id, 'competition', $postdata->competition);
             if (isset($postdata->notes)) $users->metaSet($user->id, 'notes', $postdata->notes);
             $users->metaSet($user->id, 'profile-filled', '1');
             Header::Location(SITE_LOCATION);
@@ -67,6 +67,11 @@
                     Gegevens aanvullen
                 </h1>
             </section>
+            <section class="note">
+                <p>
+                    Velden gemarkeerd met een * zijn verplicht.
+                </p>
+            </section>
             <section class="input-fields">
                 <div class="gender-selection">
                     <h3>
@@ -86,11 +91,11 @@
                     </div>
                 </div>
                 
-                <input type="number" placeholder="Leeftijd" name="age" min="12" max="99" required>
-                <input type="number" placeholder="Lengte (cm)" name="height" min="100" max="300" required>
-                <input type="text" placeholder="Vereneging" name="club" required>
-                <input type="text" placeholder="Team (u18-1)" name="team" required>
-                <input type="text" placeholder="competitie" name="competition" required>
+                <input type="number" placeholder="Leeftijd *" name="age" min="12" max="99" required>
+                <input type="number" placeholder="Lengte (cm) *" name="height" min="100" max="300" required>
+                <input type="text" placeholder="Vereneging *" name="club" required>
+                <input type="text" placeholder="Team (u18-1) *" name="team" required>
+                <input type="text" placeholder="Competitie" name="competition">
                 <textarea name="notes" id="notes" cols="30" rows="6" placeholder="Wil je nog een opmerking kwijt?"></textarea>
             </section>
             <section class="finish-signup">
