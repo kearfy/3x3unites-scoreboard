01ee1eb3f8bde85504cce206bff598442871648c
Micha de Vries <contact@michadevries.nl>
2022-03-02 15:48:35 +0100

    Patch for homepage.

--- functions/public_stats.php
+++ functions/public_stats.php
@@ -12,12 +12,14 @@
     <body>
         <?php require_once(DYNAMIC_DIR . '/modules/scoreboard/partials/navbar.php'); ?>
         
-        <div class="notready">
-            <?php echo file_get_contents(DYNAMIC_DIR . '/modules/scoreboard/static/basketball.svg'); ?>
-            <p>
-                &#9814; Helaas is de website nog niet afgerond, maar binnenkort wel &#9814;<br><br>
-                Spelers kunnen zich alvast <a href="/signup">Registreren</a>.
-            </p>
+        <div class="content">
+            <div class="notready">
+                <?php echo file_get_contents(DYNAMIC_DIR . '/modules/scoreboard/static/basketball.svg'); ?>
+                <p>
+                    Helaas is de website nog niet afgerond, maar binnenkort wel.<br><br>
+                    Spelers kunnen zich alvast <a href="/signup">Registreren</a>.
+                </p>
+            </div>
         </div>
         
         <script src="<?php echo SITE_LOCATION; ?>/pb-loader/module-static/scoreboard/default.js"></script>
